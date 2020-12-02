29ba2b1d96c7f5e6c6860af1f55f7343a0be45ee
Annika Paulheim <annika.paulheim@gmail.com>
2020-12-02 10:28:52 +0100

    day 2 py

---
+++ py/Day2/__init__.py
@@ -0,0 +1,45 @@
+import logging
+import azure.functions as func
+import json
+import re
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    data = req.params.get('input')
+    if not data:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            data = req_body.get('input')
+
+    lines = data.split(",")
+    silver = 0
+    gold = 0
+    for i in lines:
+        matchObj = re.match(r'([0-9])-([0-9]) ([a-z]): ([a-z]*)', i)
+        if matchObj:
+            matches = matchObj.groups()
+            lower = int(matches[0])
+            upper = int(matches[1])
+            letter = matches[2]
+            pw = matches[3]
+
+            regex = re.compile(letter)
+            occ = regex.findall(pw)
+            if len(occ) > 0 and (lower <= len(occ) <= upper):  
+                silver += 1
+
+            pos1 = pw[lower-1] == letter
+            pos2 = pw[upper-1] == letter
+
+            if pos1 != pos2:
+                gold += 1
+
+    result = { "silver": str(silver), "gold": str(gold) }
+    if input:
+        return func.HttpResponse(json.dumps(result))
+    else:
+        return func.HttpResponse(json.dumps(result))
