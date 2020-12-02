1d95241cde747d4397048e7a5fe3248c8f5ac6c9
Annika Paulheim <annika.paulheim@gmail.com>
2020-12-02 07:50:08 +0100

    day 2 js regex

--- js/Day2/index.ts
+++ js/Day2/index.ts
@@ -10,23 +10,22 @@ const httpTrigger: AzureFunction = async function (
   let silver = 0;
   let gold = 0;
 
+  const regexofdoom = /(?<lower>[0-9])-(?<upper>[0-9]) (?<letter>[a-z]): (?<pw>[a-z]*)/;
+
   for (const i of input) {
-    const space = i.split(" ");
-    const range = space[0].split("-");
-    const letter = space[1].split(":")[0];
-    const lower = range[0];
-    const upper = range[1];
-    const pw = space[2];
+    const data = i.match(regexofdoom).groups;
+    if (data) {
+      const regex = new RegExp(data.letter, "g");
+      const found = data.pw.match(regex);
+      if (found && found.length >= data.lower && found.length <= data.upper)
+        silver++;
+    }
 
-    const regex = new RegExp(letter, "g");
-    const found = pw.match(regex);
-    if (found && found.length >= lower && found.length <= upper) silver++;
-
-    const pos1 = pw.charAt(lower - 1) == letter;
-    const pos2 = pw.charAt(upper - 1) == letter;
-
+    const pos1 = data.pw.charAt(data.lower - 1) == data.letter;
+    const pos2 = data.pw.charAt(data.upper - 1) == data.letter;
     if (pos1 != pos2) gold++;
   }
+
   const responseMessage = `{ "silver": "${silver}", "gold": "${gold}" }`;
   context.res = {
     // status: 200, /* Defaults to 200 */
